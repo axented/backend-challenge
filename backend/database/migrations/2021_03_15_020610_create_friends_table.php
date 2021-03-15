53e54546e2c8a756943f11f7120600529a54d3f2
MiguelED <migueled2695@gmail.com>
2021-03-14 20:11:23 -0600

    Migraciones Base de datos

---
+++ backend/database/migrations/2021_03_15_020610_create_friends_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFriendsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('friends', function (Blueprint $table) {
+            $table->id();            
+            $table->integer('id_blogger');
+            $table->foreign('id_blogger')->references('id')->on('users')->onUpdate('cascade');
+            $table->integer('id_friend');
+            $table->foreign('id_friend')->references('id')->on('users')->onUpdate('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('friends');
+    }
+}
