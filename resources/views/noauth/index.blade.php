30e25797d0bd57c714b743659021ea22124eb912
MiguelED <migueled2695@gmail.com>
2021-03-17 04:48:59 -0600

    Pagina de index

---
+++ resources/views/noauth/index.blade.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+    <style>
+        .jumbotron{
+            height: 40.8rem;
+            padding-bottom: 6rem;
+            padding-top: 10.5rem;
+            background-image: url(img/1.jpg);
+            border-radius: 0rem;
+            background-repeat: no-repeat;
+            background-attachment: scroll;
+            background-position: center center;
+            background-size: cover;
+            text-align: right;
+            margin-bottom: 0px;
+            color: white;
+        }
+        .display-4{
+            margin-top: 5rem;
+        }
+        .my-4{
+            background-color: white;
+        }
+    </style>
+</head>
+<body>
+
+    <div class="jumbotron text-left">
+        <h1 class="display-4">Blogger</h1>
+        <p class="lead">API REST básica para administrar sus bloggers favoritos</p>
+        <hr class="my-4">
+        <p>Back-end challenge</p>
+    </div>
+
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -13,5 +13,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 Route::get('/', function () {
-    return 'Hello World';
+    return view('noauth.index');
 });
